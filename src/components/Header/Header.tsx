a4af9f673980748192c41d2e521e35f7489bf9e7
shuvam86 <shubhamguptakmr@gmail.com>
2024-03-14 23:10:48 +0530

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { Container } from './styles'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { NavHashLink, HashLink } from 'react-router-hash-link'
 import { useState } from 'react'
-import Resume from '../../assets/Shuvam_Gupta_Resume.pdf'
+import Resume from '../../assets/Shuvam_Gupta_resume.pdf'
 export function Header() {
   const [isActive, setActive] = useState(false)
   function toggleTheme() {
